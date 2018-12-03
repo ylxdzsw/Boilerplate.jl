6a7b20804cbb35b181d404540400f8e97ca5b895
ylxdzsw <ylxdzsw@gmail.com>
2018-12-03 19:41:16 +0800

    better example

--- src/Boilerplate.jl
+++ src/Boilerplate.jl
@@ -50,43 +50,43 @@ function web_display(port=6677, echarts=true)
                 <script src="http://echarts.baidu.com/dist/echarts.min.js"></script>
                 <script src="http://echarts.baidu.com/asset/theme/dark.js"></script>
             """
+
+            plot(json::String, id=randstring(12)) = AS("text/html", """
+                <figure id="$id" style="width:600px; height:360px; margin: 0;"></figure>
+                <script> echarts.init(document.getElementById('$id'), 'dark').setOption($json) </script>
+            """)
+
+            const echarts_examples = (
+                line = md"""
+                ### (multi-) Line Plot
+
+                ```
+                plot(@json \"""{
+                    backgroundColor: 'transparent',
+                    title: { text: 'Line Plot' },
+                    xAxis: { type: 'value' },
+                    yAxis: { type: 'value' },
+                    legend: { width: 400 },
+                    tooltip: { trigger: 'axis' },
+                    dataZoom: [{
+                        type: 'slider',
+                        start: 80,
+                        height: 20,
+                        bottom: 12
+                    }],
+                    series: [{
+                        type: 'line',
+                        name: 'rtt',
+                        smooth: true,
+                        data: [[2,3],
+                            [4,5],
+                            [6,6]]
+                    }]
+                }\""")
+                ```
+                """,
+            )
         end
-
-        plot(json::String, id=randstring(12)) = AS("text/html", """
-            <figure id="$id" style="width:600px; height:360px; margin: 0;"></figure>
-            <script> echarts.init(document.getElementById('$id'), 'dark').setOption($json) </script>
-        """)
-
-        const echarts_examples = (
-            line = md"""
-            ### (multi-) Line Plot
-
-            ```
-            plot(@json \"""{
-                backgroundColor: 'transparent',
-                title: { text: 'Line Plot' },
-                xAxis: { type: 'value' },
-                yAxis: { type: 'value' },
-                legend: { width: 400 },
-                tooltip: { trigger: 'axis' },
-                dataZoom: [{
-                    type: 'slider',
-                    start: 80,
-                    height: 20,
-                    bottom: 12
-                }],
-                series: [{
-                    type: 'line',
-                    name: 'rtt',
-                    smooth: true,
-                    data: [[2,3],
-                           [4,5],
-                           [6,6]]
-                }]
-            }\""")
-            ```
-            """,
-        )
     end)
 
     Boilerplate
