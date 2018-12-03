975d277b6a74c81daf8d739adce60da03020a89b
ylxdzsw <ylxdzsw@gmail.com>
2018-12-03 15:17:51 +0800

    init

---
+++ readme.md
@@ -0,0 +1,12 @@
+Boilerplate
+===========
+
+### Install
+
+```
+julia> ] # this brings you to the `pkg>` prompt
+(v1.0) pkg> add Restful#2877b87c0d97e9c83a60e44ed5144c3a
+(v1.0) pkg> add https://github.com/ylxdzsw/WebDisplay.jl
+(v1.0) pkg> add https://github.com/ylxdzsw/JsonBuilder.jl
+(v1.0) pkg> add https://github.com/ylxdzsw/Boilerplate.jl
+```
---
+++ src/Boilerplate.jl
@@ -0,0 +1,77 @@
+module Boilerplate
+
+function load_std()
+    Core.eval(Main, quote
+        using Base64
+        using CRC32c
+        using Dates
+        using DelimitedFiles
+        using Distributed
+        using FileWatching
+        using Future
+        using InteractiveUtils
+        using Libdl
+        using LinearAlgebra
+        using Logging
+        using Markdown
+        using Mmap
+        using Pkg
+        using Printf
+        using Profile
+        using REPL
+        using Random
+        using SHA
+        using Serialization
+        using SharedArrays
+        using Sockets
+        using SparseArrays
+        using Statistics
+        using Test
+        using UUIDs
+        using Unicode
+
+        using OhMyJulia
+        using JsonBuilder
+    end)
+end
+
+function web_display(port=6677, echarts=true)
+    withenv("WEB_DISPLAY_THEME" => "dark",
+            "WEB_DISPLAY_NOTEXT" => "true",
+            "WEB_DISPLAY_PORT" => port) do
+        Core.eval(Main, quote
+            using WebDisplay
+
+            if $echarts
+                WebDisplay.extra_header[] = """
+                    <script src="http://echarts.baidu.com/dist/echarts.min.js"></script>
+                    <script src="http://echarts.baidu.com/asset/theme/dark.js"></script>
+                """
+            end
+
+            plot(json::String, id=randstring(12)) = AS("text/html", """
+                <figure id="$id" style="width:600px; height:360px; margin: 0;"></figure>
+                <script> echarts.init(document.getElementById('$id'), 'dark').setOption($json) </script>
+            """)
+        end)
+    end
+end
+
+function sqlite_connect(path=":memory:")
+    # TODO: Sqlite REPL mode
+    Core.eval(Main, quote
+        using SQLite
+
+        const db = SQLite.DB(path)
+        q(x...) = SQLite.query(db, x...) |> DataFrame
+        macro q_str(x)
+            :( SQLite.query(db, $x) |> DataFrame )
+        end
+    end)
+end
+
+# TODO: on exit
+# 1) save specified variables into a JLD bundle
+# 2) if WebDisplay enabled and session not interactive (`julia --color=yes < main.jl > /dev/null`), notify and keep alive
+
+end # module
